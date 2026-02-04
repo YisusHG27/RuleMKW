bfc1ad9feb79e08d22cd076df271b6ee2d284056
Jesús <jahernandezg20@educarex.es>
2026-02-04 02:42:06 +0100

    Cosas para docker

--- db/init.sql
+++ db/init.sql
@@ -3,9 +3,11 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-02-2026 a las 02:30:59
+-- Tiempo de generación: 04-02-2026 a las 02:08:26
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
+CREATE DATABASE IF NOT EXISTS rulemkw;
+USE rulemkw;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
